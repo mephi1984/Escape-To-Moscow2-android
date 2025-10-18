c38529698cff7ed99a7df0eefa7d544378ea2d0d
Vladislav Khorev <mephi1984@gmail.com>
2025-10-18 20:12:10 +0300

    First version, with just apk

---
+++ app/src/main/assets/x-README.md
@@ -0,0 +1,9 @@
+# Escape-to-Moscow2
+Escape to Moscow 2 game source code.
+
+Download the game from Steam: https://store.steampowered.com/app/3821160/Pobeg_v_Moskvu_2/
+
+# License
+Code: MIT
+
+Art and Music: CC-BY
---
+++ renpyandroid/src/main/java/org/renpy/android/Constants.java
@@ -0,0 +1,12 @@
+package org.renpy.android;
+
+public class Constants {
+    // Used by the in-app purchasing code.
+    public static String store = "none";
+
+    // The names of the asset packs to check and download.
+
+    public static String assetPacks[ ] = { };
+
+
+}
